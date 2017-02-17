997cdaf2e9992047e7c0332512e1ac82c4574e61
aaplmath <jrrotella@icloud.com>
2017-02-17 16:55:20 -0500

    Add position-based auton skeleton and better drive train control mode switching

--- src/main/java/org/usfirst/frc/team4373/robot/commands/teleop/DriveWithJoystick.java
+++ src/main/java/org/usfirst/frc/team4373/robot/commands/teleop/DriveWithJoystick.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4373.robot.commands.teleop;
 
+import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4373.robot.OI;
@@ -86,6 +87,8 @@ public class DriveWithJoystick extends PIDCommand {
 
     @Override
     protected void initialize() {
+        this.driveTrain.switchControlMode(CANTalon.TalonControlMode.Speed);
+        
         this.setSetpoint(0);
         this.setInputRange(-180, 180);
         this.getPIDController().setOutputRange(-1, 1);
