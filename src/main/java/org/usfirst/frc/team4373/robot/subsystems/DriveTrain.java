165ec09e49efa2602b5c30ca3d43a2fbd6301d20
aaplmath <jrrotella@icloud.com>
2017-02-17 16:13:26 -0500

    Add untested velocity closed loop code to DriveTrain

--- src/main/java/org/usfirst/frc/team4373/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team4373/robot/subsystems/DriveTrain.java
@@ -19,6 +19,8 @@ public class DriveTrain extends Subsystem {
     private CANTalon middle1;
     private CANTalon middle2;
 
+    private CANTalon.TalonControlMode controlMode;
+
     private static DriveTrain driveTrain = null;
 
     public static DriveTrain getDriveTrain() {
@@ -38,27 +40,77 @@ public class DriveTrain extends Subsystem {
         this.middle1 = new CANTalon(RobotMap.MIDDLE_DRIVE_MOTOR_1);
         this.middle2 = new CANTalon(RobotMap.MIDDLE_DRIVE_MOTOR_2);
 
-        this.right1.enableBrakeMode(true);
-        this.right2.enableBrakeMode(true);
-        this.left1.enableBrakeMode(true);
-        this.left2.enableBrakeMode(true);
-        this.middle1.enableBrakeMode(true);
-        this.middle2.enableBrakeMode(true);
+        configureTalon(this.left1);
+        configureTalon(this.left2);
+        configureTalon(this.right1);
+        configureTalon(this.right2);
+        configureTalon(this.middle1);
+        configureTalon(this.middle2);
 
-        this.right2.changeControlMode(CANTalon.TalonControlMode.Follower);
-        this.right2.set(RobotMap.RIGHT_DRIVE_MOTOR_1);
+        this.controlMode = CANTalon.TalonControlMode.Speed;
+        this.left1.changeControlMode(controlMode);
+        this.right1.changeControlMode(controlMode);
+        this.middle1.changeControlMode(controlMode);
+
         this.left2.changeControlMode(CANTalon.TalonControlMode.Follower);
         this.left2.set(RobotMap.LEFT_DRIVE_MOTOR_1);
+        this.right2.changeControlMode(CANTalon.TalonControlMode.Follower);
+        this.right2.set(RobotMap.RIGHT_DRIVE_MOTOR_1);
         this.middle2.changeControlMode(CANTalon.TalonControlMode.Follower);
         this.middle2.set(RobotMap.MIDDLE_DRIVE_MOTOR_1);
     }
+
+    /**
+     * Configures a CANTalon to work with velocity closed loop.
+     * @param talon The CANTalon to configure.
+     */
+    private void configureTalon(CANTalon talon) {
+        talon.enableBrakeMode(true);
+
+        int absolutePosition = talon.getPulseWidthPosition() & 0xFFF; /* mask out the bottom12 bits, we don't care about the wrap arounds */
+        // Use the low level API to set the quad encoder signal
+        talon.setEncPosition(absolutePosition);
+        // Set sensor and sensor direction
+        talon.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+        talon.reverseSensor(false);
+        // Set peak and nominal outputs—12V means full
+        talon.configNominalOutputVoltage(+0d, -0d);
+        talon.configPeakOutputVoltage(+12d, -12d);
+        // Set the allowable closed-loop error—Closed-Loop output will be neutral in this range
+        talon.setAllowableClosedLoopErr(0); // Always servo
+        // Set closed loop gains in slot 0
+        talon.setProfile(0);
+        // PID config
+        talon.setF(0.0);
+        talon.setP(0.1);
+        talon.setI(0.0);
+        talon.setD(0.0);
+    }
+
+    /**
+     * Sets power to the motors.
+     * @param forward The forward power (-1=backward to 1=forward).
+     * @param right The left/right power (-1=left to 1=right).
+     */
+    public void move(double forward, double right) {
+        // TODO: Determine appropriate amount of power to deliver to right/left motors
+        // TODO: Figure out if we even need this method
+        this.middle1.set(right);
+    }
+
+    // In "set" methods, we multiply by 500 to convert from voltage to RPM
+    // (5400 max rpm / 10:1 gear ratio)
 
     /**
      * Sets power to the left motors.
      * @param power The power to allocate to the left motors from -1 to 1.
      */
     public void setLeft(double power) {
-        this.left1.set(power);
+        if (controlMode == CANTalon.TalonControlMode.Speed) {
+            this.left1.set(power * 500);
+        } else {
+            this.left1.set(power);
+        }
     }
 
     /**
@@ -67,7 +119,11 @@ public class DriveTrain extends Subsystem {
      * @param power The power to allocate to the right motors from -1 to 1.
      */
     public void setRight(double power) {
-        this.right1.set(-power);
+        if (controlMode == CANTalon.TalonControlMode.Speed) {
+            this.right1.set(power * 500);
+        } else {
+            this.right1.set(-power);
+        }
     }
 
     /**
@@ -75,7 +131,11 @@ public class DriveTrain extends Subsystem {
      * @param power The power to allocate to the middle motor from -1 (left) to 1 (right).
      */
     public void setMiddle(double power) {
-        this.middle1.set(power);
+        if (controlMode == CANTalon.TalonControlMode.Speed) {
+            this.middle1.set(power * 500);
+        } else {
+            this.middle1.set(power);
+        }
     }
 
     /**
