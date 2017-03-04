448ccea11fc7d3e6a6db4d27846979ee6872f142
aaplmath <jrrotella@icloud.com>
2017-03-04 13:04:23 -0500

    Add auton start

--- src/main/java/org/usfirst/frc/team4373/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4373.robot.commands.auton.TimeBasedAuton;
 import org.usfirst.frc.team4373.robot.commands.auton.TimeBasedGearAuton;
+import org.usfirst.frc.team4373.robot.commands.auton.AutonDriveForward;
 import org.usfirst.frc.team4373.robot.subsystems.Climber;
 import org.usfirst.frc.team4373.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team4373.robot.subsystems.GearRelease;
@@ -44,6 +45,8 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void autonomousInit() {
+        super.autonomousInit();
+        OI.getOI().getGyro().reset();
         if (autonCommand != null) {
             autonCommand.cancel();
         }
