373f9d098a4859e3abf43afe229659d4ad5536cd
Henry Pitcairn <735tesla@gmail.com>
2017-03-16 19:24:17 -0400

    Unix is a lot more complicated (than CP/M) of course -- the typical Unix hacker can never remember what the PRINT command is called this week -- but when it gets right down to it, Unix is a glorified video game. People don't do serious work on Unix systems; they send jokes around the world on USENET or write adventure games and research papers. 		-- E. Post 		"Real Programmers Don't Use Pascal", Datamation, 7/83

--- src/main/java/org/usfirst/frc/team4373/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -379,3 +379,10 @@ the most important.
 		-- Ambrose Bierce
 // The most popular labor-saving device today is still a husband with money.
 		-- Joey Adams, "Cindy and I"
+// Unix is a lot more complicated (than CP/M) of course -- the typical Unix
+hacker can never remember what the PRINT command is called this week --
+but when it gets right down to it, Unix is a glorified video game.
+People don't do serious work on Unix systems; they send jokes around the
+world on USENET or write adventure games and research papers.
+		-- E. Post
+		"Real Programmers Don't Use Pascal", Datamation, 7/83
