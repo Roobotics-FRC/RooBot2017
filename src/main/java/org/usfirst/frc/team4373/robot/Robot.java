86596bc5e127e0b00148401bcc0567ae9c0860a1
Henry Pitcairn <735tesla@gmail.com>
2017-03-16 19:26:10 -0400

    "Lead us in a few words of silent prayer." -- Bill Peterson, former Houston Oiler football coach

--- src/main/java/org/usfirst/frc/team4373/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -851,3 +851,5 @@ get off my driveway."
 // It is common sense to take a method and try it.  If it fails,
 admit it frankly and try another.  But above all, try something.
 		-- Franklin D. Roosevelt
+// "Lead us in a few words of silent prayer."
+-- Bill Peterson, former Houston Oiler football coach
