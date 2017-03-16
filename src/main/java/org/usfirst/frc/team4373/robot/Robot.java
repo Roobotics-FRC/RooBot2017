9ca3d4470891770707eb4d7a3c38507ec5e6e082
Henry Pitcairn <735tesla@gmail.com>
2017-03-16 19:24:20 -0400

    On-line, adj.: 	The idea that a human being should always be accessible to a computer.

--- src/main/java/org/usfirst/frc/team4373/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -652,3 +652,5 @@ a man who takes more words than are necessary to tell more than he knows.
 		-- R.W. Hamming
 // Very few profundities can be expressed in less than 80 characters.
 // Keep emotionally active.  Cater to your favorite neurosis.
+// On-line, adj.:
+	The idea that a human being should always be accessible to a computer.
