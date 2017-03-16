65418b84aa089fbe1af9ca146b7432f6933aac72
Henry Pitcairn <735tesla@gmail.com>
2017-03-16 19:24:21 -0400

    A critic is a bundle of biases held loosely together by a sense of taste. 		-- Whitney Balliett

--- src/main/java/org/usfirst/frc/team4373/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -733,3 +733,5 @@ or not; but my faculties are decaying now and soon I shall be so I
 cannot remember any but the things that never happened.  It is sad to
 go to pieces like this but we all have to do it.
 		-- Mark Twain
+// A critic is a bundle of biases held loosely together by a sense of taste.
+		-- Whitney Balliett
