a08cf013d6137fed04615e9e0f69301fdc1133eb
Henry Pitcairn <735tesla@gmail.com>
2017-03-16 19:24:21 -0400

    But Rabshakeh said unto them, Hath my master sent me to thy master, and to thee, to speak these words?  Hath he not sent me to the men which sit on the wall, that they may eat their own dung, and drink their own piss with you? [2 Kings 18:27 (KJV)]

--- src/main/java/org/usfirst/frc/team4373/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -742,3 +742,7 @@ around the moon it would not make a pennyworth of difference to me or my work.
 		-- Sherlock Holmes, "A Study in Scarlet"
 // A New York City ordinance prohibits the shooting of rabbits from the
 rear of a Third Avenue street car -- if the car is in motion.
+// But Rabshakeh said unto them, Hath my master sent me to thy master, and to
+thee, to speak these words?  Hath he not sent me to the men which sit on the
+wall, that they may eat their own dung, and drink their own piss with you?
+[2 Kings 18:27 (KJV)]
