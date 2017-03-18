365fb8e23cd40866b35983f12c8e15ca899a2371
Alex Fang <frjalex@temple.edu>
2017-03-18 12:09:20 -0400

    Add encoder helper methods to DriveTrain

--- src/main/java/org/usfirst/frc/team4373/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team4373/robot/subsystems/DriveTrain.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team4373.robot.subsystems;
 
 import com.ctre.CANTalon;
+import com.ctre.CanTalonJNI;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4373.robot.RobotMap;
 import org.usfirst.frc.team4373.robot.commands.teleop.DriveWithJoystick;
 import org.usfirst.frc.team4373.robot.commands.teleop.DriveWithJoystick.Direction;
@@ -38,6 +41,8 @@ public class DriveTrain extends Subsystem {
         this.middle1 = new CANTalon(RobotMap.MIDDLE_DRIVE_MOTOR_1);
         this.middle2 = new CANTalon(RobotMap.MIDDLE_DRIVE_MOTOR_2);
 
+        this.left1.reverseOutput(true);
+
         this.right1.enableBrakeMode(true);
         this.right2.enableBrakeMode(true);
         this.left1.enableBrakeMode(true);
@@ -51,13 +56,120 @@ public class DriveTrain extends Subsystem {
         this.left2.set(RobotMap.LEFT_DRIVE_MOTOR_1);
         this.middle2.changeControlMode(CANTalon.TalonControlMode.Follower);
         this.middle2.set(RobotMap.MIDDLE_DRIVE_MOTOR_1);
+
+        this.left1.configEncoderCodesPerRev(4096);
+        this.right1.configEncoderCodesPerRev(4096);
+        this.middle2.configEncoderCodesPerRev(4096);
     }
+
+    /**
+     * Changes the control mode of all leader talons.
+     * @param controlMode the control mode to change them to.
+     */
+    public void changeControlMode(CANTalon.TalonControlMode controlMode) {
+        this.left1.changeControlMode(controlMode);
+        this.right1.changeControlMode(controlMode);
+        this.middle1.changeControlMode(controlMode);
+        this.left1.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
+        this.right1.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
+        this.middle1.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
+        this.left1.enableControl();
+        this.right1.enableControl();
+        this.middle1.enableControl();
+    }
+
+    /**
+     * Returns the position (number of revolutions away from 0) of the left motors.
+     * @return The number of rotations (positive or negative) of the left motors.
+     */
+    public int getLeftEncoderPosition() {
+        return -left1.getEncPosition();
+    }
+
+    /**
+     * Returns the position (number of revolutions away from 0) of the right motors.
+     * @return The number of rotations (positive or negative) of the right motors.
+     */
+    public int getRightEncoderPosition() {
+        return right1.getEncPosition();
+    }
+
+    /**
+     * Returns the position (number of revolutions away from 0) of the middle motors.
+     * @return The number of rotations (positive or negative) of the middle motors.
+     */
+    public int getMiddleEncoderPosition() {
+        return middle1.getEncPosition();
+    }
+
+    /**
+     * Gets the number of encoder counts per revolution of the left motors.
+     * @return the number of encoder counts per revolution of the left motors.
+     */
+    public double getLeftEncoderCPR() {
+        return this.left1.getParameter(CanTalonJNI.param_t.eNumberEncoderCPR);
+    }
+
+    /**
+     * Gets the number of encoder counts per revolution of the right motors.
+     * @return the number of encoder counts per revolution of the right motors.
+     */
+    public double getRightEncoderCPR() {
+        return this.right1.getParameter(CanTalonJNI.param_t.eNumberEncoderCPR);
+    }
+
+    /**
+     * Gets the number of encoder counts per revolution of the middle motors.
+     * @return the number of encoder counts per revolution of the middle motors.
+     */
+    public double getMiddleEncoderCPR() {
+        return this.middle1.getParameter(CanTalonJNI.param_t.eNumberEncoderCPR);
+    }
+
+    /**
+     * Gets the number of times the left wheels have rotated.
+     * @return the number of times the left wheels have rotated.
+     */
+    public double getLeftEncoderTurns() {
+        return getLeftEncoderPosition() / getLeftEncoderCPR();
+    }
+
+    /**
+     * Gets the number of times the right wheels have rotated.
+     * @return the number of times the right wheels have rotated.
+     */
+    public double getRightEncoderTurns() {
+        return getRightEncoderPosition() / getRightEncoderCPR();
+    }
+
+    /**
+     * Gets the number of times the middle wheels have rotated.
+     * @return the number of times the middle wheels have rotated.
+     */
+    public double getMiddleEncoderTurns() {
+        return getMiddleEncoderPosition() / getMiddleEncoderCPR();
+    }
+
 
     /**
      * Sets power to the left motors.
      * @param power The power to allocate to the left motors from -1 to 1.
      */
     public void setLeft(double power) {
+        if (this.left1.getControlMode().equals(CANTalon.TalonControlMode.Position)) {
+            SmartDashboard.putBoolean("setLeftTalonPosition", true);
+            power *= getLeftEncoderCPR();
+            this.left1.setSetpoint(power);
+        }
+        SmartDashboard.putNumber("leftTalonValue", power);
+        DriverStation.reportError("leftTalonValue, "
+                + power + "\tCPR:"
+                + getLeftEncoderCPR() + "\n",
+                true);
+        if (power == 0) {
+            Exception e = new Exception("Oh fuck");
+            e.printStackTrace();
+        }
         this.left1.set(power);
     }
 
@@ -67,6 +179,9 @@ public class DriveTrain extends Subsystem {
      * @param power The power to allocate to the right motors from -1 to 1.
      */
     public void setRight(double power) {
+        if (this.right1.getControlMode().equals(CANTalon.TalonControlMode.Position)) {
+            power *= getRightEncoderCPR();
+        }
         this.right1.set(-power);
     }
 
@@ -75,7 +190,19 @@ public class DriveTrain extends Subsystem {
      * @param power The power to allocate to the middle motor from -1 (left) to 1 (right).
      */
     public void setMiddle(double power) {
+        if (this.middle1.getControlMode().equals(CANTalon.TalonControlMode.Position)) {
+            power *= getMiddleEncoderCPR();
+        }
         this.middle1.set(power);
+    }
+
+    /**
+     * Resets all encoder positions.
+     */
+    public void resetEncoders() {
+        this.left1.setEncPosition(0);
+        this.right1.setEncPosition(0);
+        this.middle1.setEncPosition(0);
     }
 
     /**
@@ -121,7 +248,7 @@ public class DriveTrain extends Subsystem {
 
     @Override
     protected void initDefaultCommand() {
-        setDefaultCommand(DriveWithJoystick.getDriveWithJoystick());
+        // setDefaultCommand(DriveWithJoystick.getDriveWithJoystick());
     }
 
 }
